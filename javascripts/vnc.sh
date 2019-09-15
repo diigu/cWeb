e3a34499fd75b2c72db2f09f690c2ec0879a8673
WhoamiMiss <5822468+wlya@users.noreply.github.com>
2019-09-15 10:22:42 +0800

    Update vnc.sh

--- javascripts/vnc.sh
+++ javascripts/vnc.sh
@@ -20,7 +20,7 @@ else
     sudo apt-get install -y    apt-transport-https    ca-certificates    curl    gnupg2    software-properties-common  > /dev/null 2>&1
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
     sudo add-apt-repository   "deb [arch=amd64] https://download.docker.com/linux/debian   $(lsb_release -cs)   stable"
-    sudo apt-get update > /dev/null 2>&
+    sudo apt-get update > /dev/null 2>&1
     echo "Apt updated, install Docker-ce"
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io  > /dev/null 2>&1
     echo "Docker-ce installed, pulling VNC Container"
